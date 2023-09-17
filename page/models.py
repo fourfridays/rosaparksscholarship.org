f25f6fd34507453927e18aafa2ed14fd7348f878
Umair Abbasi <umair.abbasi@fourfridays.com>
2023-09-16 21:16:17 -0400

    dropped blocks from home models

--- page/models.py
+++ page/models.py
@@ -7,23 +7,13 @@ from page.blocks import ImageGridBlock, SingleColumnBlock, TwoColumnBlock, Three
 
 
 class HomePage(Page):
-    body = StreamField([
-        ('single_column', SingleColumnBlock(group='COLUMNS')),
-        ('two_columns', TwoColumnBlock(group='COLUMNS')),
-        ('three_columns', ThreeColumnBlock(group='COLUMNS')),
-        ('four_columns', FourColumnBlock(group='COLUMNS')),
-        ('image_grid', ImageGridBlock(icon='image', min_num=2, max_num=4, help_text='Minimum 2 blocks and a maximum of 4 blocks')),
-    ], use_json_field=True, default='')
+    pass
 
     # Restricting the creation of this page type in settings
     is_creatable = settings.WAGTAIL_PAGES_IS_CREATABLE
 
     # Restricting the page from being created anywhere other than root
     parent_page_types = ['wagtailcore.Page']
-
-    content_panels = Page.content_panels + [
-        FieldPanel('body'),
-    ]
 
 
 class StandardPage(Page):
