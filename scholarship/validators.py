adefd2b9c154d4f3cdcdc697a39e50dfb066c1f7
Umair Abbasi <umair.abbasi@fourfridays.com>
2023-11-28 10:20:33 -0500

    raised attachment size to 3MB and MiB to MB

--- scholarship/validators.py
+++ scholarship/validators.py
@@ -4,9 +4,9 @@ from django.core.exceptions import ValidationError
 
 
 def file_size(value):
-    limit = 1 * 1024 * 1024
+    limit = 3 * 1024 * 1024
     if value.size > limit:
-        raise ValidationError("File too large. Size should not exceed 1 MiB")
+        raise ValidationError("File too large. Size should not exceed 3 MB")
 
 
 def validate_document_file_extension(content):
