6249d1804cd160467667769c17964bb1b3aca387
Umair Abbasi <umair.abbasi@fourfridays.com>
2023-12-31 12:55:38 -0500

    set sentry log level to info
    Maybe alot of noise? We will find out!

--- settings.py
+++ settings.py
@@ -70,6 +70,9 @@ sentry_dsn = os.environ.get("SENTRY_DSN", "")
 if sentry_dsn:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk import set_level
+    
+    set_level("info")
 
     def ignore_disallowedhost(event, hint):
         if event.get("logger", None) == "django.security.DisallowedHost":
